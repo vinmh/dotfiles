d9c329cf4123c6ff04253e6df4fc8adb7b93d757
vinmh <vinmh@proton.me>
2024-06-10 23:58:24 -0400

    Auto-update from 2024-06-10

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -36,7 +36,15 @@ require("lazy").setup({
   "williamboman/mason.nvim", -- Package Manager
   "williamboman/mason-lspconfig.nvim", -- LSP Package Installer
   "neovim/nvim-lspconfig",
-  "nvim-tree/nvim-tree.lua",
+  {"nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
--- nvim/lua/tree.lua
+++ nvim/lua/tree.lua
@@ -1,5 +1,7 @@
-require("nvim-tree").setup()
+require("neo-tree").setup()
 
 -- Keybindings
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<Leader>t', ':NvimTreeToggle<CR>', opts)
+vim.keymap.set('n', '<Leader>t', ':Neotree reveal toggle<CR>', opts)
+vim.keymap.set('n', '<Leader>g', ':Neotree git_status toggle float<CR>', opts)
+vim.keymap.set('n', '<Leader>\\', ':Neotree buffers toggle right<CR>', opts)
